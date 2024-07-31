8091fc6fabfbc820b52cf0a12aedacf0db8dde63
Josh Klope <klope3@msn.com>
2024-07-31 07:01:04 -0500

    Add basic player hit detection

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -5,6 +5,21 @@ using BNG;
 
 public class Testing : MonoBehaviour
 {
+    [SerializeField] private GameObject hitPlayerVisual;
+    [SerializeField] private LayerMask playerLayer;
+
+    private void Update()
+    {
+        bool hit = Physics.Raycast(new Ray(transform.position, transform.forward), out RaycastHit hitInfo, int.MaxValue, playerLayer);
+        if (hit)
+        {
+            hitPlayerVisual.SetActive(true);
+        } else
+        {
+            hitPlayerVisual.SetActive(false);
+        }
+    }
+
     public void ReceiveKnobEvent(float degrees)
     {
         VRUtils.Instance.Log($"Knob turned to {degrees} degrees");
