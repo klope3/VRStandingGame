d1c8c6cee0d88564843ba3abd16137ba88749b59
Josh Klope <klope3@msn.com>
2024-08-04 19:38:51 -0500

    Add basic enemy attack

--- Assets/Scripts/AiBrain.cs
+++ Assets/Scripts/AiBrain.cs
@@ -9,7 +9,11 @@ public class AiBrain : MonoBehaviour
     [SerializeField] private float moveTimer;
     [SerializeField, Tooltip("When choosing a new navTarget, the max angle away from its current position, relative to the world center.")] 
         private float maxAngleDeviance;
+    [SerializeField, Tooltip("Frequency of the trig function used to determine attack/non-attack times.")] private float attackFrequency;
+    [SerializeField, Tooltip("Vertical offset of the trig function used to determine attack/non-attack times.")] private float attackOffset;
+    [SerializeField] private ProjectileLauncher launcher;
     private float timer;
+    private float attackTime; //used as the X value for evaluating the attack trig function.
 
     private void Awake()
     {
@@ -24,5 +28,16 @@ public class AiBrain : MonoBehaviour
             movement.SetNavTarget(positionProvider.GetRandPosition(transform.position, maxAngleDeviance));
             timer = moveTimer;
         }
+
+        attackTime += Time.deltaTime;
+        if (IsAttacking()) Debug.Log("Attack");
+        launcher.SetTrigger(IsAttacking());
+    }
+
+    // The "sometimes attacking, sometimes not" behavior can be defined with a simple trig function.
+    // When it returns greater than 0, we're attacking.
+    private bool IsAttacking()
+    {
+        return Mathf.Sin(attackFrequency * attackTime) + attackOffset > 0;
     }
 }
--- Assets/Scripts/AiMovement.cs
+++ Assets/Scripts/AiMovement.cs
@@ -7,15 +7,28 @@ public class AiMovement : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float thrustForce;
     [SerializeField] private float navTargetTolerance; //upon getting within this distance of navTarget, will decide that it has "reached" the target
+    [SerializeField, Tooltip("The transform that will be rotated to visually 'look' at the lookTarget.")] 
+        private Transform lookingTransform;
+    [SerializeField, Tooltip("Find the GameObject with this tag on Awake to use as the lookTarget")] 
+        private string lookTargetTag;
+    private Transform lookTarget;
     private Vector3 navTarget;
     private bool reachedNavTarget; //set to false when given a new navTarget, and set to true when within the tolerance distance
 
     private void Awake()
     {
         navTarget = transform.position;
+        GameObject lookGo = GameObject.FindGameObjectWithTag(lookTargetTag);
+        if (lookGo == null) Debug.LogWarning($"GameObject with tag ${lookTargetTag} not found!");
+        else lookTarget = lookGo.transform;
     }
 
     private void Update()
+    {
+        lookingTransform.LookAt(lookTarget);
+    }
+
+    private void FixedUpdate()
     {
         if (reachedNavTarget) return;
 
