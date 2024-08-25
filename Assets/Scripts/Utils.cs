737c8e4744e6347178c3447d0ff8c4802083874e
Josh Klope <klope3@msn.com>
2024-08-25 07:17:31 -0500

    Add gun saber trail

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils
+{
+    public static string Vec3String(Vector3 vec)
+    {
+        return $"({vec.x}, {vec.y}, {vec.z})";
+    }
+}
