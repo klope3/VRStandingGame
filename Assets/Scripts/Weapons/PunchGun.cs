ec367af8106f4ec960f729a13d78c5c67101b8fd
Josh Klope <klope3@msn.com>
2024-09-17 06:51:57 -0500

    Decouple punch gun speed detection from frame rate

--- Assets/Scripts/Weapons/PunchGun.cs
+++ Assets/Scripts/Weapons/PunchGun.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PunchGun : MonoBehaviour
 {
-    [SerializeField, Tooltip("How strict to be about only triggering punches on forward movement. At 1, only perfect forward movement triggers a punch. At 0, even perfect sideways movement can trigger a punch.")]
-        private float punchStrictness;
-    [SerializeField, Tooltip("How far the muzzle needs to travel between frames to activate a shot.")] 
-        private float punchTravel;
+    [SerializeField]
+    [Tooltip("How strict to be about only triggering punches on forward movement. At 1, only perfect forward movement triggers a punch. At 0, even perfect sideways movement can trigger a punch.")]
+    private float punchStrictness;
+    
+    [SerializeField]
+    [Tooltip("How fast the muzzle needs to travel to activate a shot.")]
+    private float punchSpeed;
+    
     [SerializeField] private Transform muzzlePoint;
     [SerializeField] private ProjectileLauncher launcher;
     private Vector3 positionLastFrame;
@@ -15,8 +19,10 @@ public class PunchGun : MonoBehaviour
     private void Update()
     {
         Vector3 moveVec = muzzlePoint.position - positionLastFrame;
+        float distTraveled = moveVec.magnitude;
+        float actualUnitsPerSecond = distTraveled / Time.deltaTime;
         float dot = Vector3.Dot(moveVec.normalized, muzzlePoint.forward);
-        bool punch = dot > punchStrictness && moveVec.magnitude > punchTravel;
+        bool punch = dot > punchStrictness && actualUnitsPerSecond > punchSpeed;
         launcher.SetTrigger(punch);
 
         positionLastFrame = muzzlePoint.position;
